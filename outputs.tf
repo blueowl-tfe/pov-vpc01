449a8d70785a33c38b2b50fdd2e3164a46629fe0
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-06-25 07:47:02 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "vpc_id" {
   value = "${aws_vpc.vpc.id}"
 }
+
+output "subnet-public-1_id" {
+  value = "${aws_subnet.subnet-public-1.id}"
+}
+
+output "subnet-public-2_id" {
+  value = "${aws_subnet.subnet-public-2.id}"
+}
+
+output "subnet-public-3_id" {
+  value = "${aws_subnet.subnet-public-3.id}"
+}
